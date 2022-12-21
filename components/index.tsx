2a73d88e9d1e582fcf5d918caa8ef7f882a8ab94
Ikenna <iykee0333@gmail.com>
2022-12-21 10:22:13 +0000

    set username modal

--- components/index.tsx
+++ components/index.tsx
@@ -2,3 +2,7 @@ export { default as Navbar } from "./Navbar";
 export { default as Banner } from "./Banner";
 export { default as Footer } from "./Footer";
 export { default as Loader } from "./Loader";
+export { default as Message } from "./Message";
+export { default as UsernameModal } from "./GetStartedModal";
+export { default as GetStartedModal } from "./UsernameModal";
+export { default as MessagesWrapper } from "./MessagesWrapper";
