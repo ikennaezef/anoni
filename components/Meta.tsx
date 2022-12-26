08bf186ddd428ea82bb3cc5f50c6fe0949b90574
Ikenna <iykee0333@gmail.com>
2022-12-26 17:31:46 +0000

    meta

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -10,6 +10,22 @@ const Meta = ({ title }: MetaProps) => {
 		<Head>
 			<meta charSet="UTF-8" />
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<link rel="shortcut icon" href="/favicon.ico" />
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="32x32"
+				href="/images/anoni.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="16x16"
+				href="/images/anoni.png"
+			/>
+
+			<link rel="apple-touch-icon" sizes="180x180" href="/images/anoni.png" />
+			<link rel="mask-icon" href="/images/anoni.png" color="grey"></link>
 			<meta
 				name="keywords"
 				content="anonymous, anon, messaging, secret message"
